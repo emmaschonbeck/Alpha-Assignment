2d9aecd2852dc37eaa8bcec501a56fb2c11f7d9f
Emma <emmaschonbeck7@gmail.com>
2025-04-09 16:29:49 +0200

    Add project files.

---
+++ WebApp/Controllers/ProjectsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+    [Route("Projects")]
+    public class ProjectsController : Controller
+    {
+        [Route("")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+}
+
